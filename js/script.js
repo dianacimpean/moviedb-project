13cc0e7066b8ffdd0fa9f9ef5196cf6b655db25d
dianacimpean <32507346+dianacimpean@users.noreply.github.com>
2017-10-30 11:29:46 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 var apiKey = "a8b96d868accfb62d4a1c7cec3d74994";
 
+$('#spinner').hide();
+
 function getDetails(idLink) {
     var id = $(idLink).attr('data-value');
     console.log(id);
+    
+     $('#spinner').show();
      $.ajax({
             method: "GET",
             url: "https://api.themoviedb.org/3/movie/" + id + "?api_key=a8b96d868accfb62d4a1c7cec3d74994&language=en-US",
@@ -11,12 +15,15 @@ function getDetails(idLink) {
             success: function(result){         
             let movieDetails = {data:JSON.parse(result),trigger:'Data'};
                 
+            $('#spinner').hide();
+                
             $('#modal-title').empty();
             $('#lang').empty();
             $('#release').empty();
             $('#descript').empty();
             $('#budget').empty();
             $('#revenue').empty();
+            $('#genres').empty();
                 
             $('#modal-title').append("Movie title: " + movieDetails.data.title);
             $('#lang').append("Language: " + movieDetails.data.original_language);    
@@ -24,7 +31,17 @@ function getDetails(idLink) {
             $('#descript').append("Description: " + movieDetails.data.overview);
             $('#budget').append("Budget: " + movieDetails.data.budget + " $");
             $('#revenue').append("Revenue: " + movieDetails.data.revenue + " $");
-                
+                if (movieDetails.data.genres.length > 0) {
+                for (var i = 0; i <= movieDetails.data.genres.length - 1; i++ ){
+                    if (i === movieDetails.data.genres.length - 1) {
+                        $("#genres").append(movieDetails.data.genres[i].name); 
+                    } else {
+                   $("#genres").append(movieDetails.data.genres[i].name + ", ");
+                    }
+                }
+                } else {
+                    $("#genres").append(movieDetails.data.genres.name);
+                }
             },
            error: function (jqXHR, errorText, error) {
                  toastr.error('Network error has occurred, please try again!');
@@ -42,28 +59,46 @@ function truncateText(selector, maxLength) {
                     return truncated;
                 }
 
+function truncateNumber(selector, maxLength) {
+                    var element = document.querySelector(selector),
+                        truncated = element.innerText;
+
+                    if (truncated.length > maxLength) {
+                        truncated = truncated.substr(0,maxLength) + '...' + maxLength + ">";
+                    }
+                    return truncated;
+                }
+
 function searchByYear() { 
     
+    $('#spinner').show();
     $.ajax({
            method: "GET",
-           url: "https://api.themoviedb.org/3/discover/movie?api_key=a8b96d868accfb62d4a1c7cec3d74994&include_adult=false&include_video=false&page=1&year=" + $('#byYear').val(),
+           url: "https://api.themoviedb.org/3/discover/movie?api_key=a8b96d868accfb62d4a1c7cec3d74994&include_adult=false&include_video=false&page=1&year=" + $('#byYear').val() + "&page=1",
            dataType: 'text',
            contentType: "application/json",
            success: function(result){
            let movieResults = {data:JSON.parse(result),trigger:'Data'};
-                
+               $('#spinner').hide();
                $('#results').empty();
+               $('#total_results').empty();
+               $('#total_pages').empty();
                
-               $("#total_results").append("Total Results: " + movieResults.data.total_results + " movies");
+               $('#total_results').append("Total Results: " + movieResults.data.total_results + " movies");
                
                for (var i=0; i< movieResults.data.results.length; i++ ){  
                 
-                $("#results").append("<div class='media' onClick='getDetails(this);' data-value='" + movieResults.data.results[i].id + "'data-toggle='modal' data-target='#myModal'> <div class='media-left'> <img class='media-object' style='width:110px' src='http://image.tmdb.org/t/p/w92/" + movieResults.data.results[i].poster_path + "'/> </div><div class='media-body'> <h4 class='media-heading'>" + movieResults.data.results[i].title + "</h4> <p class='language'>" + movieResults.data.results[i].original_language + "</p> <p class='release_date'> |  " + movieResults.data.results[i].release_date + "</p> <p class='description'>" + movieResults.data.results[i].overview + "</p> <p class='total_votes'> |  Total votes: " + movieResults.data.results[i].vote_count +"</p><p class='vote_average'> Rating: " + movieResults.data.results[i].vote_average + "</p> </div> </div> <br>");
-                
+                $('#results').append("<div class='media' onClick='getDetails(this);' data-value='" + movieResults.data.results[i].id + "'data-toggle='modal' data-target='#myModal'> <div class='media-left'> <img class='media-object' style='width:110px' src='http://image.tmdb.org/t/p/w92/" + movieResults.data.results[i].poster_path + "'/> </div><div class='media-body'> <h4 class='media-heading'>" + movieResults.data.results[i].title + "</h4> <p class='language'>" + movieResults.data.results[i].original_language + "</p> <p class='release_date'> |  " + movieResults.data.results[i].release_date + "</p> <p class='description'>" + movieResults.data.results[i].overview + "</p> <p class='total_votes'> |  Total votes: " + movieResults.data.results[i].vote_count +"</p><p class='vote_average'> Rating: " + movieResults.data.results[i].vote_average + "</p> </div> </div> <br>");
+                                               
                 document.querySelector('.description').innerText = truncateText('.description', 100);
                 
-                $("#results").change(movieResults);
+                $('#results').change(movieResults);
                 }
+               
+               for( var i=1; i <= movieResults.data.total_pages; i++){
+                   $('#pageNumbers').append("<a href=''><span id='pageNr' value=" + i + " onClick='switchPage(this);'>" + i + "</span></a> ");
+                   document.querySelector('#pageNumbers').innerText = truncateNumber('#pageNumbers', 4);
+               }
                
             },
            error: function (jqXHR, errorText, error) {
@@ -72,18 +107,55 @@ function searchByYear() {
     });
 }
 
+function switchPage(valueLink) { 
+    var pagea = $(valueLink).attr('value');
 
-function searchByKeyword() {
+    $('#spinner').show();
+    $.ajax({
+           method: "GET",
+           url: "https://api.themoviedb.org/3/discover/movie?api_key=a8b96d868accfb62d4a1c7cec3d74994&include_adult=false&include_video=false&page=" + pagea + "&year=" + $('#byYear').val(),
+           dataType: 'text',
+           contentType: "application/json",
+           success: function(result){
+               
+           let movieResults = {data:JSON.parse(result),trigger:'Data'};
+               
+               $('#spinner').hide();
+               $('#results').empty();
+               $('#total_results').empty();
+               $('#total_pages').empty();
+               
+               $('#total_results').append("Total Results: " + movieResults.data.total_results + " movies");
+               
+               for (var i=0; i< movieResults.data.results.length; i++ ){  
+                
+                $('#results').append("<div class='media' onClick='getDetails(this);' data-value='" + movieResults.data.results[i].id + "'data-toggle='modal' data-target='#myModal'> <div class='media-left'> <img class='media-object' style='width:110px' src='http://image.tmdb.org/t/p/w92/" + movieResults.data.results[i].poster_path + "'/> </div><div class='media-body'> <h4 class='media-heading'>" + movieResults.data.results[i].title + "</h4> <p class='language'>" + movieResults.data.results[i].original_language + "</p> <p class='release_date'> |  " + movieResults.data.results[i].release_date + "</p> <p class='description'>" + movieResults.data.results[i].overview + "</p> <p class='total_votes'> |  Total votes: " + movieResults.data.results[i].vote_count +"</p><p class='vote_average'> Rating: " + movieResults.data.results[i].vote_average + "</p> </div> </div> <br>");
+                                               
+                document.querySelector('.description').innerText = truncateText('.description', 100);
+                
+                $('#results').change(movieResults);
+                }
+            },
+           error: function (jqXHR, errorText, error) {
+           },
+    });
+}
+
+
+function searchByTitle() {
+    
+    $('#spinner').show();
     $.ajax({
         method: "GET",
-        url: "https://api.themoviedb.org/3/search/movie?api_key=a8b96d868accfb62d4a1c7cec3d74994&language=en-US&query=" + $('#byKeyword').val() + "&page=1&include_adult=false",
+        url: "https://api.themoviedb.org/3/search/movie?api_key=a8b96d868accfb62d4a1c7cec3d74994&language=en-US&query=" + $('#byTitle').val() + "&page=1&include_adult=false",
         dataType: 'text',
         contentType: "application/json",
         success: function(result){         
         let movieResults = {data:JSON.parse(result),trigger:'Data'};
-                
+            $('#spinner').hide();   
             $('#results').empty();
-               
+            $("#total_results").empty();
+            $("#total_pages").empty();
             $("#total_results").append("Total Results: " + movieResults.data.total_results + " movies");
                 
             for (var i=0; i< movieResults.data.results.length; i++ ){  
@@ -100,71 +172,6 @@ function searchByKeyword() {
         },
     });
 }
-
-
-// function searchByGenre() {
-//     $.ajax({
-//            method: "GET",
-//            url: testUrl,
-//            dataType: 'text',
-//            contentType: "application/json",
-//            success: function(result){
-//            let movieResults = {data:JSON.parse(result),trigger:'Data'};
-//               for (var i=0; i< movieResults.data.results.length; i++ ){  
-//                 $('#results').empty();
-//                 $("#results").append("<div class='media'> <div class='media-left'> <img class='media-object' style='width:60px' src='http://image.tmdb.org/t/p/w92/" + movieResults.data.results[i].poster_path + "'/> </div><div class='media-body' id='" + movieResults.data.results[i].id + "'> <h4 class='media-heading'>asta-i titlu:" + movieResults.data.results[i].title + "</h4> <p> " + movieResults.data.results[i].original_language + "</p> <p>" + movieResults.data.results[i].release_date + "</p> <p class='description'>" + movieResults.data.results[i].overview + "</p> <br> <button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal' onClick='getId();'>Open Modal</button> <p class='vote_average'>" + movieResults.data.results[i].vote_average + "</p> </div> </div> <br>");  
-                
-//                 $("#results").change(movieResults);
-//                 }
-//             },
-//            error: function (jqXHR, errorText, error) {
-//                  toastr.error('Network error has occurred please try again!');
-//            },
-//     });
-// }
-
-// function searchByActor() {
-//     $.ajax({
-//            method: "GET",
-//            url: testUrl,
-//            dataType: 'text',
-//            contentType: "application/json",
-//            success: function(result){
-//            let movieResults = {data:JSON.parse(result),trigger:'Data'};
-//               for (var i=0; i< movieResults.data.results.length; i++ ){  
-//                 $('#results').empty();  
-//                 $("#results").append("<div class='media'> <div class='media-left'> <img class='media-object' style='width:60px' src='http://image.tmdb.org/t/p/w92/" + movieResults.data.results[i].poster_path + "'/> </div><div class='media-body' id='" + movieResults.data.results[i].id + "'> <h4 class='media-heading'>asta-i titlu:" + movieResults.data.results[i].title + "</h4> <p> " + movieResults.data.results[i].original_language + "</p> <p>" + movieResults.data.results[i].release_date + "</p> <p class='description'>" + movieResults.data.results[i].overview + "</p> <br> <button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal' onClick='getId();'>Open Modal</button> <p class='vote_average'>" + movieResults.data.results[i].vote_average + "</p> </div> </div> <br>");  
-                
-//                 $("#results").change(movieResults);
-//                 }
-//             },
-//            error: function (jqXHR, errorText, error) {
-//                  toastr.error('Network error has occurred please try again!');
-//            },
-//     });
-// }
-
-// function searchByReleaseDate() {
-//     $.ajax({
-//            method: "GET",
-//            url: testUrl,
-//            dataType: 'text',
-//            contentType: "application/json",
-//            success: function(result){
-//            let movieResults = {data:JSON.parse(result),trigger:'Data'};
-//               for (var i=0; i< movieResults.data.results.length; i++ ){  
-//                 $('#results').empty();  
-//                 $("#results").append("<div class='media'> <div class='media-left'> <img class='media-object' style='width:60px' src='http://image.tmdb.org/t/p/w92/" + movieResults.data.results[i].poster_path + "'/> </div><div class='media-body' id='" + movieResults.data.results[i].id + "'> <h4 class='media-heading'>asta-i titlu:" + movieResults.data.results[i].title + "</h4> <p> " + movieResults.data.results[i].original_language + "</p> <p>" + movieResults.data.results[i].release_date + "</p> <p class='description'>" + movieResults.data.results[i].overview + "</p> <br> <button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal' onClick='getId();'>Open Modal</button> <p class='vote_average'>" + movieResults.data.results[i].vote_average + "</p> </div> </div> <br>");  
-                
-//                 $("#results").change(movieResults);
-//                 }
-//             },
-//            error: function (jqXHR, errorText, error) {
-//                  toastr.error('Network error has occurred please try again!');
-//            },
-//     });
-// }
-
 
 
 /*
