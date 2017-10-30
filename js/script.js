e925b7baf1f59591f8d1d8a2f565c42a72b5ece5
dianacimpean <32507346+dianacimpean@users.noreply.github.com>
2017-10-30 16:21:41 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -49,25 +49,15 @@ function getDetails(idLink) {
     });
 }
 
-function truncateText(selector, maxLength) {
-                    var element = document.querySelector(selector),
-                        truncated = element.innerText;
-
-                    if (truncated.length > maxLength) {
-                        truncated = truncated.substr(0,maxLength) + '...';
-                    }
-                    return truncated;
-                }
-
-function truncateNumber(selector, maxLength) {
-                    var element = document.querySelector(selector),
-                        truncated = element.innerText;
-
-                    if (truncated.length > maxLength) {
-                        truncated = truncated.substr(0,maxLength) + '...' + maxLength + ">";
-                    }
-                    return truncated;
-                }
+//function truncateNumber(selector, maxLength) {
+//                    var element = document.querySelector(selector),
+//                        truncated = element.innerText;
+//
+//                    if (truncated.length > maxLength) {
+//                        truncated = truncated.substr(0,maxLength) + ' ...'+ " >";
+//                    }
+//                    return truncated;
+//                }
 
 function searchByYear() { 
     
@@ -88,16 +78,16 @@ function searchByYear() {
                
                for (var i=0; i< movieResults.data.results.length; i++ ){  
                 
-                $('#results').append("<div class='media' onClick='getDetails(this);' data-value='" + movieResults.data.results[i].id + "'data-toggle='modal' data-target='#myModal'> <div class='media-left'> <img class='media-object' style='width:110px' src='http://image.tmdb.org/t/p/w92/" + movieResults.data.results[i].poster_path + "'/> </div><div class='media-body'> <h4 class='media-heading'>" + movieResults.data.results[i].title + "</h4> <p class='language'>" + movieResults.data.results[i].original_language + "</p> <p class='release_date'> |  " + movieResults.data.results[i].release_date + "</p> <p class='description'>" + movieResults.data.results[i].overview + "</p> <p class='total_votes'> |  Total votes: " + movieResults.data.results[i].vote_count +"</p><p class='vote_average'> Rating: " + movieResults.data.results[i].vote_average + "</p> </div> </div> <br>");
-                                               
-                document.querySelector('.description').innerText = truncateText('.description', 100);
+                   var shortDescription = jQuery.trim(movieResults.data.results[i].overview).substring(0, 100).trim(this) + "..."
                 
+                $('#results').append("<div class='media' onClick='getDetails(this);' data-value='" + movieResults.data.results[i].id + "'data-toggle='modal' data-target='#myModal'> <div class='media-left'> <img class='media-object' style='width:110px' src='http://image.tmdb.org/t/p/w92/" + movieResults.data.results[i].poster_path + "'/> </div><div class='media-body'> <h4 class='media-heading'>" + movieResults.data.results[i].title + "</h4> <p class='language'>" + movieResults.data.results[i].original_language + "</p> <p class='release_date'> |  " + movieResults.data.results[i].release_date + "</p> <p class='description'>" + shortDescription + "</p> <p class='total_votes'> |  Total votes: " + movieResults.data.results[i].vote_count +"</p><p class='vote_average'> Rating: " + movieResults.data.results[i].vote_average + "</p> </div> </div> <br>");
+                   
                 $('#results').change(movieResults);
                 }
                
                for( var i=1; i <= movieResults.data.total_pages; i++){
                    $('#pageNumbers').append("<a href=''><span id='pageNr' value=" + i + " onClick='switchPage(this);'>" + i + "</span></a> ");
-                   document.querySelector('#pageNumbers').innerText = truncateNumber('#pageNumbers', 4);
+//         document.querySelector('#pageNumbers').innerText = truncateNumber('#pageNumbers', 4);
                }
                
             },
@@ -129,9 +119,9 @@ function switchPage(valueLink) {
                
                for (var i=0; i< movieResults.data.results.length; i++ ){  
                 
-                $('#results').append("<div class='media' onClick='getDetails(this);' data-value='" + movieResults.data.results[i].id + "'data-toggle='modal' data-target='#myModal'> <div class='media-left'> <img class='media-object' style='width:110px' src='http://image.tmdb.org/t/p/w92/" + movieResults.data.results[i].poster_path + "'/> </div><div class='media-body'> <h4 class='media-heading'>" + movieResults.data.results[i].title + "</h4> <p class='language'>" + movieResults.data.results[i].original_language + "</p> <p class='release_date'> |  " + movieResults.data.results[i].release_date + "</p> <p class='description'>" + movieResults.data.results[i].overview + "</p> <p class='total_votes'> |  Total votes: " + movieResults.data.results[i].vote_count +"</p><p class='vote_average'> Rating: " + movieResults.data.results[i].vote_average + "</p> </div> </div> <br>");
-                                               
-                document.querySelector('.description').innerText = truncateText('.description', 100);
+                   var shortDescription = jQuery.trim(movieResults.data.results[i].overview).substring(0, 100).trim(this) + "..."
+                
+                $('#results').append("<div class='media' onClick='getDetails(this);' data-value='" + movieResults.data.results[i].id + "'data-toggle='modal' data-target='#myModal'> <div class='media-left'> <img class='media-object' style='width:110px' src='http://image.tmdb.org/t/p/w92/" + movieResults.data.results[i].poster_path + "'/> </div><div class='media-body'> <h4 class='media-heading'>" + movieResults.data.results[i].title + "</h4> <p class='language'>" + movieResults.data.results[i].original_language + "</p> <p class='release_date'> |  " + movieResults.data.results[i].release_date + "</p> <p class='description'>" + shortDescription + "</p> <p class='total_votes'> |  Total votes: " + movieResults.data.results[i].vote_count +"</p><p class='vote_average'> Rating: " + movieResults.data.results[i].vote_average + "</p> </div> </div> <br>");
                 
                 $('#results').change(movieResults);
                 }
@@ -160,9 +150,9 @@ function searchByTitle() {
                 
             for (var i=0; i< movieResults.data.results.length; i++ ){  
                   
-                $("#results").append("<div class='media' onClick='getDetails(this);' data-value='" + movieResults.data.results[i].id + "'data-toggle='modal' data-target='#myModal'> <div class='media-left'> <img class='media-object' style='width:110px' src='http://image.tmdb.org/t/p/w92/" + movieResults.data.results[i].poster_path + "'/> </div><div class='media-body'> <h4 class='media-heading'>" + movieResults.data.results[i].title + "</h4> <p class='language'>" + movieResults.data.results[i].original_language + "</p> <p class='release_date'> |  " + movieResults.data.results[i].release_date + "</p> <p class='description'>" + movieResults.data.results[i].overview + "</p> <p class='total_votes'> |  Total votes: " + movieResults.data.results[i].vote_count +"</p><p class='vote_average'> Rating: " + movieResults.data.results[i].vote_average + "</p> </div> </div> <br>");
+                var shortDescription = jQuery.trim(movieResults.data.results[i].overview).substring(0, 100).trim(this) + "..."
                 
-                document.querySelector('.description').innerText = truncateText('.description', 100);
+                $('#results').append("<div class='media' onClick='getDetails(this);' data-value='" + movieResults.data.results[i].id + "'data-toggle='modal' data-target='#myModal'> <div class='media-left'> <img class='media-object' style='width:110px' src='http://image.tmdb.org/t/p/w92/" + movieResults.data.results[i].poster_path + "'/> </div><div class='media-body'> <h4 class='media-heading'>" + movieResults.data.results[i].title + "</h4> <p class='language'>" + movieResults.data.results[i].original_language + "</p> <p class='release_date'> |  " + movieResults.data.results[i].release_date + "</p> <p class='description'>" + shortDescription + "</p> <p class='total_votes'> |  Total votes: " + movieResults.data.results[i].vote_count +"</p><p class='vote_average'> Rating: " + movieResults.data.results[i].vote_average + "</p> </div> </div> <br>");
                 
                 $("#results").change(movieResults);
                 }
@@ -172,166 +162,3 @@ function searchByTitle() {
         },
     });
 }
-
-
-/*
- * jQuery Pagination
- * Author: Austin Wulf (@austinwulf)
- *
- * Call the paginate method on an array
- * of elements. Accepts # of items per page
- * as an argument. Defaults to 5.
- *
- * Example:
- *     $(selector).paginate(3);
- *
- * Released under the MIT License.
- *
- * v 1.0
- */
-//
-//(function($){
-//    
-//    var paginate = {
-//        startPos: function(pageNumber, perPage) {
-//            // determine what array position to start from
-//            // based on current page and # per page
-//            return pageNumber * perPage;
-//        },
-//
-//        getPage: function(items, startPos, perPage) {
-//            // declare an empty array to hold our page items
-//            var page = [];
-//
-//            // only get items after the starting position
-//            items = items.slice(startPos, items.length);
-//
-//            // loop remaining items until max per page
-//            for (var i=0; i < perPage; i++) {
-//                page.push(items[i]); }
-//
-//            return page;
-//        },
-//
-//        totalPages: function(items, perPage) {
-//            // determine total number of pages
-//            return Math.ceil(items.length / perPage);
-//        },
-//
-//        createBtns: function(totalPages, currentPage) {
-//            // create buttons to manipulate current page
-//            var pagination = $('<div class="pagination" />');
-//
-//            // add a "first" button
-//            pagination.append('<span class="pagination-button">&laquo;</span>');
-//
-//            // add pages inbetween
-//            for (var i=1; i <= totalPages; i++) {
-//                // truncate list when too large
-//                if (totalPages > 5 && currentPage !== i) {
-//                    // if on first two pages
-//                    if (currentPage === 1 || currentPage === 2) {
-//                        // show first 5 pages
-//                        if (i > 5) continue;
-//                    // if on last two pages
-//                    } else if (currentPage === totalPages || currentPage === totalPages - 1) {
-//                        // show last 5 pages
-//                        if (i < totalPages - 4) continue;
-//                    // otherwise show 5 pages w/ current in middle
-//                    } else {
-//                        if (i < currentPage - 2 || i > currentPage + 2) {
-//                            continue; }
-//                    }
-//                }
-//
-//                // markup for page button
-//                var pageBtn = $('<span class="pagination-button page-num" />');
-//
-//                // add active class for current page
-//                if (i == currentPage) {
-//                    pageBtn.addClass('active'); }
-//
-//                // set text to the page number
-//                pageBtn.text(i);
-//
-//                // add button to the container
-//                pagination.append(pageBtn);
-//            }
-//
-//            // add a "last" button
-//            pagination.append($('<span class="pagination-button">&raquo;</span>'));
-//
-//            return pagination;
-//        },
-//
-//        createPage: function(items, currentPage, perPage) {
-//            // remove pagination from the page
-//            $('.pagination').remove();
-//
-//            // set context for the items
-//            var container = items.parent(),
-//                // detach items from the page and cast as array
-//                items = items.detach().toArray(),
-//                // get start position and select items for page
-//                startPos = this.startPos(currentPage - 1, perPage),
-//                page = this.getPage(items, startPos, perPage);
-//
-//            // loop items and readd to page
-//            $.each(page, function(){
-//                // prevent empty items that return as Window
-//                if (this.window === undefined) {
-//                    container.append($(this)); }
-//            });
-//
-//            // prep pagination buttons and add to page
-//            var totalPages = this.totalPages(items, perPage),
-//                pageButtons = this.createBtns(totalPages, currentPage);
-//
-//            container.after(pageButtons);
-//        }
-//    };
-//
-//    // stuff it all into a jQuery method!
-//    $.fn.paginate = function(perPage) {
-//        var items = $(this);
-//
-//        // default perPage to 5
-//        if (isNaN(perPage) || perPage === undefined) {
-//            perPage = 5; }
-//
-//        // don't fire if fewer items than perPage
-//        if (items.length <= perPage) {
-//            return true; }
-//
-//        // ensure items stay in the same DOM position
-//        if (items.length !== items.parent()[0].children.length) {
-//            items.wrapAll('<div class="pagination-items" />');
-//        }
-//
-//        // paginate the items starting at page 1
-//        paginate.createPage(items, 1, perPage);
-//
-//        // handle click events on the buttons
-//        $(document).on('click', '.pagination-button', function(e) {
-//            // get current page from active button
-//            var currentPage = parseInt($('.pagination-button.active').text(), 10),
-//                newPage = currentPage,
-//                totalPages = paginate.totalPages(items, perPage),
-//                target = $(e.target);
-//
-//            // get numbered page
-//            newPage = parseInt(target.text(), 10);
-//            if (target.text() == '«') newPage = 1;
-//            if (target.text() == '»') newPage = totalPages;
-//
-//            // ensure newPage is in available range
-//            if (newPage > 0 && newPage <= totalPages) {
-//                paginate.createPage(items, newPage, perPage); }
-//        });
-//    };
-//
-//})(jQuery);
-//
-///* This part is just for the demo,
-//not actually part of the plugin */
-//$('.article-loop').paginate(2);
